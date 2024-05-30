cfd97b64a02d7c5edd80d37ac9d0e45dd73b3748
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-05-30 18:43:26 +0700

    Fix typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -172,7 +172,7 @@
 					id="avatar"
 					bind:files={updateUserAvatar}
 					type="file"
-					accept="image/jped,image/png,image/svg+xml,image/gif,image/webp,image/avif"
+					accept="image/jpeg,image/png,image/svg+xml,image/gif,image/webp,image/avif"
 					class="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 				/>
 			</div>
